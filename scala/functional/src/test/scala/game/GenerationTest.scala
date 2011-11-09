55c58ac795e6b89e75157e949d5981b0a22627f3
Grzegorz Dziemidowicz <dziemid@rybnet.pl>
2011-11-09 21:00:33 +0100

    Lonely cell

--- scala/functional/src/test/scala/game/GenerationTest.scala
+++ scala/functional/src/test/scala/game/GenerationTest.scala
@@ -1,7 +1,18 @@
 package game
 
+import org.junit._
+import org.junit.Assert._
+import org.hamcrest.core.Is._
+
 class GenerationTest {
 
+  @Test def lonelyCellShouldDie() {
+    def lonelyCell = Generation(Seq(Position(1, 1)))
+
+    val nextGeneration = lonelyCell.next
+
+    assertThat(nextGeneration.aliveCells.size, is(0));
+  }
 
 
 }
